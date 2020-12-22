2da9df1b8ef25676b666e0133f6ce7827a048d36
Zane Peycke <z@peyck.es>
2020-12-22 07:42:05 -0500

    adding summarization back

--- run-gradio.py
+++ run-gradio.py
@@ -1,12 +1,12 @@
 import gradio as gr
-# from summarizer import Summarizer
-# import torch
+from summarizer import Summarizer
+import torch
 
 
 def summarize_text(text):
-    # model = Summarizer()
-    # return model(text)
-    return('Test working')
+    model = Summarizer()
+    return model(text)
+    # return('Test working')
 
 inputs = gr.inputs.Textbox(lines=10, placeholder="Paste Text Here...")
 output_text = gr.outputs.Textbox()
